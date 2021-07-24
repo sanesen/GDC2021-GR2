da845f5c62cd8b64881aca90ca5d1e095127b9d8
GDC2021-GR1-PROG <teknisk@game.unf.dk>
2021-07-24 14:05:24 +0200

    Timer og points

--- GDC2021MegaPack/Assets/Scripts/Bullet.cs
+++ GDC2021MegaPack/Assets/Scripts/Bullet.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private GameManager gameManager;
+
     Rigidbody RBbullet;
     public float bulletSpeed;
+    int bulletBounces;
+    public int maxBulletBounces;
 
     void Awake()
     {
+        gameManager = FindObjectOfType<GameManager>();
         RBbullet = GetComponent<Rigidbody>();
         RBbullet.AddForce(transform.forward * bulletSpeed, ForceMode.VelocityChange);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag=="Player")
+        if (collision.gameObject.tag == "Player1")
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
+            gameManager.tank1Destroyed = true;
+        }
+        else if (collision.gameObject.tag == "Player2")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            gameManager.tank2Destroyed = true;
+        }
+        else
+        {
+            if (bulletBounces <= maxBulletBounces)
+            {
+                bulletBounces++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
         }
     }
 }
--- GDC2021MegaPack/Assets/Scripts/GameManager.cs
+++ GDC2021MegaPack/Assets/Scripts/GameManager.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance;
+    public GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    
+    public bool tank1Destroyed, tank2Destroyed;
+
+    int point1, point2;
+    float timer;
+    public float gameTime;
+    public TextMeshProUGUI pointText1, pointText2;
+    public TextMeshProUGUI timerText;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        timer = gameTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (tank1Destroyed)
+        {
+            point2++;
+            pointText2.text = point2.ToString();
+            print(point2);
+            tank1Destroyed = false;
+        }
+
+        if (tank2Destroyed)
+        {
+            point1++;
+            pointText1.text = point1.ToString();
+            tank2Destroyed = false;
+        }
+
+        timer -= Time.deltaTime;
+        timerText.text = timer.ToString("F0");
     }
 }
